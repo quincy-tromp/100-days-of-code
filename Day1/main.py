3a9cf1e9e27b0303d4de3aaaf477096ab974a317
Quincy Tromp <95527406+quincy-tromp@users.noreply.github.com>
2022-05-18 21:40:27 +0200

    Update main.py

--- Day1/main.py
+++ Day1/main.py
@@ -1,14 +1,8 @@
-#Day 1 of 100 - Band Name Generator
+#Band Name Generator
 
-#Greeting
 print("Welcome to the Band Name Generator!")
 
-#Question 1
 city = input("What's the name of the city you grew up in?\n")
-
-#Question 2
 pet = input("What's your pet's name?\n")
-
-#Band name
 band_name = city + " " + pet
 print("Your band name could be" + " " + band_name)
