d58c0a1ef393b335121f47114b45f073f1cd4189
Quincy Tromp <95527406+quincy-tromp@users.noreply.github.com>
2022-05-19 22:27:26 +0200

    Update and rename exercises.py to notes.py

--- Day2/notes.py
+++ Day2/notes.py
@@ -1,15 +1,12 @@
-#Data Types
-
-#1 String
-
+#Data types
+#String
 print("Hello"[0])
 
-#2 Integer
+#Integer
 print(123 + 345)
-
 print(123_456_789)
 
-#3 Float
+#Float
 3.1419
 
 #Boolean
@@ -21,27 +18,23 @@ num_char = len(input("What is your name? "))
 print(type(num_char))
 
 new_num_char = str(num_char)
-
 print("Your name has " + new_num_char + " characters.")
 
-#Code challenge 2.1
+#Code challenge 
 two_digit_number = input("Type a two digit number. ")
-
 first_digit = two_digit_number[0]
 second_digit = two_digit_number[1]
-
 new_digit = int(first_digit) + int(second_digit)
 print("Your new digit number is: " + str(new_digit))
 
-#Code challenge 2.2 - BMI
-
+#bmi
 weight = float(input("What is your weight? (kg)"))
 height = float(input("What is your height? (cm)"))
 bmi = weight / (height / 100) ** 2
 bmi_as_int = round(bmi, 2)
 print("Your BMI is: " + str(bmi_as_int))
 
-#f-string
+#f-strings
 score = 15
 print(f"Your score is {score}")
 
@@ -53,6 +46,5 @@ age_difference = age_to_reach - age_as_int
 months_left = age_difference * 12
 weeks_left = age_difference * 52
 days_left = age_difference * 365
-
 message = f"You have {days_left} days, {weeks_left} weeks, and {months_left} months left."
 print(message)
