7de4ceb49a3d7736088d9cd72f406564173af797
Quincy Tromp <quincy.tromp@hotmail.com>
2022-06-08 09:55:13 +0200

    Completed Day 20

---
+++ day20/README.md
@@ -0,0 +1,3 @@
+<h1>Day 20 - Build the Snake Game Part 1: Animation & Coordinates</h1>
+<h2>Project: Snake Game (Part 1)</h2>
+<img src='snake-game-part-1.gif'>
---
+++ day20/main.py
@@ -0,0 +1,27 @@
+from turtle import Screen, Turtle
+from time import sleep
+from snake import Snake
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("My Snake Game")
+screen.tracer(0)
+
+snake = Snake()
+
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+
+sleep(2)
+game_on = True
+while game_on:
+    screen.update()
+    sleep(0.1)
+
+    snake.move()
+
+screen.exitonclick()
